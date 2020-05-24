665d8c4dda6f1ebb63fa49f73b4f2477e3238bef
yasinehsan <yasinehsan11@gmail.com>
2020-05-24 11:23:22 -0400

    added header cell custom

---
+++ ListHeaderViewCell.swift
@@ -0,0 +1,20 @@
+
+import UIKit
+
+class ListHeaderViewCell: UITableViewCell {
+    
+    @IBOutlet var nameLebel: UILabel!
+    @IBOutlet var pfpImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ grocery/View Design/ListHeaderViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ListHeaderViewCell.swift
+//  grocery
+//
+//  Created by Yasin Ehsan on 5/24/20.
+//  Copyright © 2020 Helal. All rights reserved.
+//
+
+import Foundation
